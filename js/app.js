c566fa8a57dd797d35c323e3cd88565eed54e601
Jou M. <djem@hotmail.com>
2025-09-24 00:41:34 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -268,7 +268,7 @@ $("#btnSwap").removeClass('btn-muted');
 
 $( document ).ready(function() {
   getWalletAddress();
-  connectButton.addEventListener('click', connectWallet);
+  $("$connectButton").addEventListener('click', connectWallet);
     
 var accountInterval = setInterval(function() {
   if ('tronWeb' in window) {
